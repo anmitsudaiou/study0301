880a9e87627b6dbfce747fc9028e15390c10bdc7
anmitsudaiou <yikor1504@gmail.com>
2022-03-01 14:44:24 +0900

    (Q-3)Q-3の完了

---
+++ question3.js
@@ -0,0 +1,49 @@
+/**
+ *  メイン処理
+ *  lines: Array<string> 入力された行(末尾は必ず改行)分の配列
+ */
+ const main = () => {
+
+    //5つの数字入力
+    let quantity = 5
+    let num = []
+    for(let i = 0; i < quantity; i++){
+        if(Number(lines[i]) >= 0 && Number(lines[i] <= 100)){
+            num.push(lines[i])
+        }
+    }
+
+    const result = Math.min(...num)
+    console.log(result);
+    
+    //1行目の入力値に関する処理
+    // const calc = lines[0].split(' ')
+    // const a = Number(calc[0])
+    // const b = Number(calc[1])
+    // if(a >= 0 && a <= 100){
+    //     if(b >= 0 && b <= 100){
+    //         let result = a + b
+    //         console.log(result);
+    //     }
+    // }
+    console.log('');
+}
+
+/**
+ * 標準入出力のための処理
+ */
+const reader = require('readline')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+
+rl.on('close', main)
